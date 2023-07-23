22f4a3e0e13ca06e0d4d29c43da8539eacc82b9f
kamegoro <tentyo0620@icloud.com>
2023-07-23 23:42:21 +0900

    add: google_app_engine_standard_app_version settings

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,10 +1,14 @@
 provider "google" {
   project     = "${var.project_name}"
   region      = "${var.region}"
-  zone      = "asia-northeast1-a"
 }
 
 resource "google_app_engine_application" "app" {
   project     = "${var.project_name}"
   location_id = "asia-northeast2"
+}
+
+resource "google_app_engine_standard_app_version" "my_work" {
+  version_id = "v1"
+  runtime = "nodejs20"
 }
